77869c40d46c3d714692c3af1200f278c28867db
JoelUwabor <joelisiomauwabor@gmail.com>
2022-09-13 14:59:21 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - Short decription, single line
+ *
+ * @c: contains value to be compared
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}

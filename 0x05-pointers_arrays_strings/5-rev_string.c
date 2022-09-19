c9735261e29e0318177e37970abea37be174fb1b
JoelUwabor <joelisiomauwabor@gmail.com>
2022-09-19 13:55:19 -0700

    new fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,16 +13,16 @@ void rev_string(char *s)
 
 
 	length1 = 0;
-	lrngth2 = 0;
+	length2 = 0;
 
 	while (s[length1] != '\0')
 		length1++;
 
-	length2 = length1 - 1
+	length2 = length1 - 1;
+
 		for (i = 0; i < length1 / 2; i++)
 		{
-			"samalx"  "xlamas"
-				tmp = s[i];
+			tmp = s[i];
 			s[i] = s[length2];
 			s[length2] = tmp;
 			length2 -= 1;
